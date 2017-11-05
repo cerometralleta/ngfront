0bcb3545dd996c0f3e94ebc926a74fd93ca5e8ea
zkning <120716625@qq.com>
2017-11-05 18:08:28 +0800

    fix:select修改

--- ubold-app/src/app/ngb/component/ngbDropdown.component.ts
+++ ubold-app/src/app/ngb/component/ngbDropdown.component.ts
@@ -1,11 +1,11 @@
-import { OnInit, Input, Component, forwardRef, Renderer2, ElementRef } from '@angular/core';
+import { OnInit, Input, Component, forwardRef, Renderer2, ElementRef, ViewChild } from '@angular/core';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { HttpService } from '../../frame/service/http.service';
 import { GoldbalConstant } from '../../sm/constant/global.constant';
 import { Application } from '../../sm/constant/application.constant';
 import { ControlValueAccessor, FormGroup, FormBuilder, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 
-export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
+export const INPUT_CONTROL_VALUE_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
   useExisting: forwardRef(() => NgbDropdownComponent),
   multi: true
@@ -13,15 +13,17 @@ export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
 
 @Component({
     selector: 'ng4b-dropdown',
-    template: ` <select class="form-control"
+    template: ` <select class="form-control" #selectInput
                 [ngModel]="_selectValue"
                 [minlength]="minlength"
                 [maxlength]="maxlength"
                 [disabled]="disabled"
+                (change)="selectValue($event)"
                 >
-                <option *ngFor="let code of datalist" [value]="code">{{datalist[code]}}</option>
+                <option *ngFor="let data of datalist" [value]="data.code">{{data.text}}</option>
                 </select>
-              `
+              `,
+    providers: [INPUT_CONTROL_VALUE_ACCESSOR]
 })
 export class NgbDropdownComponent implements OnInit, ControlValueAccessor {
   @Input() dataFormat: string;
@@ -29,51 +31,48 @@ export class NgbDropdownComponent implements OnInit, ControlValueAccessor {
   @Input() maxlength: number;
   @Input() disabled: boolean;
   @Input() readonly = true;
-  datalist = {};
+  datalist = [];
   private _selectValue: any = '';
-  private _onTouchedCallback: () => {};
-  private _onChangeCallback: (_: any) => {};
-  constructor(public httpService: HttpService) {
+  @ViewChild('selectInput') _elementRef: ElementRef;
+  private propagateChange = (_: any) => { };
+  private onTouched = () => null;
+  constructor(public httpService: HttpService
+    , private _renderer: Renderer2) {
   }
   ngOnInit(): void {
-      alert( 111 );
-      // if (this.dataFormat) {
-      //   const df = JSON.parse( this.dataFormat );
-      //   if ( df.code) {
-      //     this.httpService.doPost(Application.ubold_sql_get_code +  df.code, {}).subscribe(resp => {
-      //       if (GoldbalConstant.STATUS_CODE.SUCCESS === resp.code) {
-      //           // this.formControlValue = resp.result;
-      //           // this.writeValue(resp.result);
-      //           // update the form
-      //           // this.propagateChange(resp.result);
-      //       }
-      //     });
-      //   }else if (df.data) {
-      //     this.datalist = JSON.parse( df.data );
-      //   }
-      // }
-    }
-    get selectValue(): any {
-      return this._selectValue;
-    }
-    set selectValue(value: any) {
-      if (value !== this._selectValue) {
-        // this._inputValue = value;
-        this._onChangeCallback(value);
+      if (this.dataFormat) {
+        const df = JSON.parse( this.dataFormat );
+        if ( df.code) {
+          this.httpService.doPost(Application.ubold_sql_get_code +  df.code, {}).subscribe(resp => {
+            if (GoldbalConstant.STATUS_CODE.SUCCESS === resp.code) {
+                // this.formControlValue = resp.result;
+                // this.writeValue(resp.result);
+                // update the form
+                // this.propagateChange(resp.result);
+            }
+          });
+        }else if (df.data) {
+          this.datalist = JSON.parse( df.data );
+        }
       }
-      // this.hasValue = (value != null && value.length > 0)
-       this._onTouchedCallback();
+    }
+    selectValue($event) {
+       this.writeValue($event.target.value);
+       this.propagateChange($event.target.value);
     }
     // From ControlValueAccessor interface
     writeValue(value: any) {
       this._selectValue = value;
     }
     // From ControlValueAccessor interface
-    registerOnChange(fn: any) {
-      this._onChangeCallback = fn;
-    }
-    // From ControlValueAccessor interface
     registerOnTouched(fn: any) {
-      this._onTouchedCallback = fn;
+      this.onTouched = fn;
+    }
+    registerOnChange(fn: any): void {
+      this.propagateChange = fn;
+    }
+    setDisabledState(isDisabled: boolean) {
+      this.disabled = isDisabled;
+      this._renderer.setProperty(this._elementRef.nativeElement, 'disabled', isDisabled);
     }
   }
