431730c2721396745c10c177bbc760fa4bd9514a
zkning <120716625@qq.com>
2018-04-14 16:46:49 +0800

    fix:创建视图不需要权限

--- ubold-app/src/app/app.routes.ts
+++ ubold-app/src/app/app.routes.ts
@@ -50,7 +50,7 @@ const appRoutes: Routes = [
             {
                 path: 'dataviewedit/:code',
                 component: DataViewEditComponent,
-                canActivate: [ GuardService ],
+                // canActivate: [ GuardService ],
                 resolve: {
 
                     // define dataViewResolver
